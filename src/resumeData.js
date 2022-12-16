fd1ae4411f3216232eba962d012b3e5d9a14d8e6
Chiman <dekaonbest1323@gmail.com>
2022-12-17 03:04:31 +0530

    First commit

---
+++ src/resumeData.js
@@ -0,0 +1,138 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Chiman Deka",
+    "role": "Frontend Developer and Machine Learning Enthusiast",
+    "linkedinId":"www.linkedin.com/in/chiman-deka-642a8514a",
+    // "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of Web development and like to learn about new technologies, read technical articles or simply play cricket in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"http://www.linkedin.com/in/chiman-deka-642a8514a",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Chiman-Deka",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/rbhatia46",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"I am currently a pre-final year student at The Assam Engineering College and pursuing my B.Tech from here. I am a self taught Front End Web Developer, currently diving deeper into Machine Learning and Full stack Web Development. I am a very passionate geek always try to explore new things, a Programmer by hobby, and also have a keen interest in Electronics. A promising, young and energetic budding Engineer ready to uplift the community.",
+    "address":"Guwahati, Assam, India",
+    "website":"dekaonbest1323@gmail.com",
+    "education":[
+      {
+        "UniversityName":"Assam Engineering College",
+        "specialization":"Electronics and Telecommunication Department",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2024",
+        "Achievements":"B.Tech."
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Zerund Pvt. Ltd.",
+        "specialization":"React Developer Intern",
+        "MonthOfLeaving":"August 2022 - Present",
+        "YearOfLeaving":"2022",
+        "Achievements":"Responsible for developing and maintaining an admin panel website for the employees to control and monitor various transactions, availability of raw materials, daily sales of raw material etc."
+      },
+      {
+        "CompanyName":"Indian Institute Of Technology Guwahati (IITG)",
+        "specialization":"Machine Learning Intern",
+        "MonthOfLeaving":"October 2022 - Present",
+        "YearOfLeaving":"2022",
+        "Achievements":"Research and develop machine learning models for natural language processing tasks including information extraction, question answering. working in understanding and ability to implement algorithms Under the Guidance of - Professor Dr. Prithwijit Guha, IITG."
+      },
+      {
+        "CompanyName":"National Institute of Electronics & Information Technology",
+        "specialization":"Machine Learning Intern",
+        "MonthOfLeaving":"Auggust to September, 2022",
+        "YearOfLeaving":"2022",
+        "Achievements":"I had undertaken my academic internship at NEILIT Guwahati under the supervision of Shri Ranjan Kumar Deka, IT faculty in the domain of ML using Python along with a mini project on House Price Prediction"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    // "skills":[
+    //   {
+    //     "skillname":"Reactjs"
+    //   },
+    //   {
+    //     "skillname":"Javascript"
+    //   },
+    //   {
+    //     "skillname":"Python"
+    //   }
+    //   ,
+    //   {
+    //     "skillname":"C++"
+    //   }
+    // ],
+    "portfolio":[
+      {
+        "name":"Pyrokinesis 2022",
+        "description":"This is the official website of annual cultural fest of Assam engineeing College",
+        "imgurl":"images/portfolio/aec logo.png",
+        "url": "https://github.com/Chiman-Deka/pyrokinesis-2k22"
+      },
+      {
+        "name":"MERN Notebook website",
+        "description":"This is a notebook to store day to day notes. It has sign in option for multiple users and can access the notes by logged in. Notes can only be access by the user.",
+        "imgurl":"images/portfolio/mern1.jfif",
+        "url": "https://github.com/Chiman-Deka/MERN_notebook"
+      },
+      {
+        "name":"EDC frontend",
+        "description":"This is the frontend part of the Entrepreneurship Development Cell Website, Assam Engineering College",
+        "imgurl":"images/portfolio/edc.jpeg",
+        "url": "https://github.com/Chiman-Deka/EDC_frontend"
+      },
+      {
+        "name":"EDC Backend",
+        "description":"This is the backend part of the Entrepreneurship Development Cell Website, Assam Engineering College",
+        "imgurl":"images/portfolio/edc.jpeg",
+        "url": "https://github.com/Chiman-Deka/EDC_Backend"
+      },
+      {
+        "name":"House Price Prediction",
+        "description":"Predicts House Price Using machine learning model",
+        "imgurl":"images/portfolio/house.jfif",
+        "url": "https://github.com/Chiman-Deka/House-Price-Predictioin"
+      },
+      {
+        "name":"More projects",
+        "description":"Visit github",
+        "imgurl":"images/portfolio/more.png",
+        "url": "https://github.com/Chiman-Deka"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ],
+    "skills":[
+      // skills can be added or removed
+      // if there are no skills, Skills section won't show up
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'React',
+      'Redux',
+      'Python',
+      'C++',
+      'Git',
+    ]
+  }
+  
+  export default resumeData
